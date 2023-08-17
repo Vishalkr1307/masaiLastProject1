9b6d354f02fcc683e1f0053eb0d5815de7b6ca90
Vishal Kumar <131303753+Vishalkr1307@users.noreply.github.com>
2023-08-17 17:43:01 +0530

    init

---
+++ backend/src/server/server.js
@@ -0,0 +1,10 @@
+const app=require("..//index")
+const connect=require("..//config/db")
+require("dotenv").config()
+
+const port=process.env.PORT||8080
+
+app.listen(port, async (req,res)=>{
+    await connect()
+    console.log(`Connect to ${port}`)
+})
